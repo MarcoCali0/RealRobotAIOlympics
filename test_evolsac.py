de4f10259b0bf6615fad86a1582b9115d8a10023
Marco Calì <marcocali@protonmail.com>
2024-09-10 23:25:43 +0200

    major fix on test script

--- test_evolsac.py
+++ test_evolsac.py
@@ -1,5 +1,6 @@
-import sys
 import os
+import sys
+
 import gymnasium as gym
 import numpy as np
 from double_pendulum.experiments.hardware_control_loop_tmotors import run_experiment
@@ -11,8 +12,8 @@ from double_pendulum.simulation.perturbations import get_random_gauss_perturbati
 from double_pendulum.simulation.simulation import Simulator
 from stable_baselines3 import SAC
 
-from double_pendulum.controller.evolsac.evolsac_controller import EvolSACController
-#from evolsaccontroller import EvolSACController
+# from double_pendulum.controller.evolsac.evolsac_controller import EvolSACController
+from evolsaccontroller import EvolSACController
 
 robot = str(sys.argv[1])
 friction_compensation = True if str(sys.argv[2]) == "FC" else False
@@ -40,7 +41,7 @@ plant = SymbolicDoublePendulum(model_pars=mpar)
 print("Loading model parameters...")
 simulator = Simulator(plant=plant)
 
-max_velocity = 50.0 # usually 50.0
+max_velocity = 50.0  # usually 50.0
 dynamics_func = double_pendulum_dynamics_func(
     simulator=simulator,
     dt=dt,
@@ -51,21 +52,20 @@ dynamics_func = double_pendulum_dynamics_func(
     torque_limit=torque_limit,
 )
 
-#dt = 1 / 500  # 500 Hz
+# dt = 1 / 500  # 500 Hz
 control_frequency = 1 / 100  # 100 Hz controller frequency
 ctrl_rate = int(control_frequency / dt)
 
 wait_steps = 0
 
 if model_selection == 0:
-    # dovrebbe essere quello corretto per la performance leaderboard (senza attrito)
     model_path = f"models/{robot}_no_friction"
 elif model_selection == 1:
-    # modello allenato sui parametri sbagliati (con l'attrito)
     model_path = f"models/{robot}_noisy"
 elif model_selection == 2:
     model_path = f"models/{robot}_noise_trained"
 elif model_selection == 3:
+    # trained with max_velocity = 20, max_torque = 3
     model_path = f"models/{robot}_model_to_try"
 
 
@@ -73,9 +73,15 @@ if model_selection not in [2, 3]:
     sac_model = SAC.load(model_path)
 else:
     # this controller requires redefining obs and act spaces
-    obs_space = gym.spaces.Box(np.array([-1.0] * 50), np.array([1.0] * 50))
+    if model_selection in [3]:
+        obs_space = gym.spaces.Box(np.array([-1.0] * 50), np.array([1.0] * 50))
+    else:
+        obs_space = gym.spaces.Box(np.array([-1.0] * 4), np.array([1.0] * 4))
     act_space = gym.spaces.Box(np.array([-1]), np.array([1]))
-    sac_model = SAC.load(model_path, custom_objects={'observation_space': obs_space, 'action_space': act_space})
+    sac_model = SAC.load(
+        model_path,
+        custom_objects={"observation_space": obs_space, "action_space": act_space},
+    )
 
 
 controller = EvolSACController(
@@ -83,8 +89,8 @@ controller = EvolSACController(
     dynamics_func=dynamics_func,
     window_size=0,
     include_time=False,
-#    ctrl_rate=ctrl_rate,
-#    wait_steps=wait_steps,
+    ctrl_rate=ctrl_rate,
+    wait_steps=wait_steps,
 )
 
 
@@ -111,9 +117,13 @@ run_experiment(
     t_final=t_final,
     can_port="can0",
     motor_ids=[3, 1],
+    motor_directions=[1.0, -1.0],
     tau_limit=torque_limit,
-    save_dir=os.path.join("data", f"{robot}/evolsac_{model_path}_{'FC' if friction_compensation else ''}"),
+    save_dir=os.path.join(
+        "data",
+        f"{robot}/evolsac_{max_torque}Nm_{model_path}{'_FC' if friction_compensation else ''}",
+    ),
     record_video=False,
     safety_velocity_limit=30.0,
-#    perturbation_array=perturbation_array,
+    #    perturbation_array=perturbation_array,
 )
